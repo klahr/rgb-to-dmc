6b33aeaefd3df17435a91b832376656bd54617a6
Joachim Klahr <joachim.klahr@gmail.com>
2021-12-13 20:52:34 +0100

    Fixed bug with absolute values.

--- convert.go
+++ convert.go
@@ -1,19 +1,18 @@
 package rgbtodmc
 
 import (
-	"fmt"
 	"math"
 )
 
 func FindNearest(red, green, blue uint8) Color {
 	if len(Colors) == 0 {
-		panic("Init() has not been called")
+		panic("InitColors() has not been called")
 	}
 
 	var bestMatch Color
 	bestD := 1000.0
 	for _, color := range Colors {
-		r, g, b := (float64)(red-color.Red), (float64)(green-color.Green), (float64)(blue-color.Blue)
+		r, g, b := math.Abs((float64)(red)-(float64)(color.Red)), math.Abs((float64)(green)-(float64)(color.Green)), math.Abs((float64)(blue)-(float64)(color.Blue))
 		var d float64
 
 		if ((int32)(red)+(int32)(color.Red))/2 < 128 {
@@ -21,7 +20,6 @@ func FindNearest(red, green, blue uint8) Color {
 		} else {
 			d = math.Sqrt(3.0*r*r + 4.0*g*g + 2.0*b*b)
 		}
-		fmt.Printf("%f %s\n", d, color.Name)
 		if d < bestD {
 			bestD = d
 			bestMatch = color
--- convert_test.go
+++ convert_test.go
@@ -1,19 +1,33 @@
 package rgbtodmc
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestFindNearest(t *testing.T) {
+func TestFindNearest_SalmonVeryLight(t *testing.T) {
 	InitColors()
-	/*if nearest := FindNearest(255, 226, 226); nearest.Dmc != "3713" {
+	if nearest := FindNearest(255, 226, 226); nearest.Dmc != "3713" {
 		t.Fail()
 	}
+}
+
+func TestFindNearest_SnowWhite(t *testing.T) {
+	InitColors()
 	if nearest := FindNearest(255, 255, 255); nearest.Dmc != "B5200" {
 		t.Fail()
-	}*/
-	if nearest := FindNearest(255, 0, 255); nearest.Dmc != "" {
-		fmt.Print(nearest.Dmc)
+	}
+}
+
+func TestFindNearest_CyclamenPinkDark(t *testing.T) {
+	InitColors()
+	if nearest := FindNearest(255, 0, 255); nearest.Dmc != "3804" {
+		t.Fail()
+	}
+}
+
+func TestFindNearest_Black(t *testing.T) {
+	InitColors()
+	if nearest := FindNearest(0, 0, 0); nearest.Dmc != "310" {
+		t.Fail()
 	}
 }
